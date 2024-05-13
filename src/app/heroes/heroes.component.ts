c525aba803bc2b3f96bbd9ce37af3749de7c2d32
Nina Petrova <git config --petrova1nina@gmail.com>
2024-05-13 18:06:17 +0300

    feat: create hero details component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import {FormsModule} from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgFor } from '@angular/common';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
   heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
